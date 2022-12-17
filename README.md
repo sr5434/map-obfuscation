5f8b8f56dbbb91fedf3453d46d6de301a8bbef4e
Samir <118690585+sr5434@users.noreply.github.com>
2022-12-17 17:53:11 +0000

    Version 3.0.0

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # MapObfuscation
 [NPM Package](https://www.npmjs.com/package/map-obfuscation/)
+
 This package obfuscates coordinates to improve user privacy. The code has some detailed comments if you want to learn more about the inner workings of this algorithm.
 # Example Usage
 ```
 var mapObfuscation = require("map-obfuscation");//Include MapObfuscation
 console.log(mapObfuscation.wgs2sd(12.345, 67.890));//Print obfuscated coordinates in the console
+```
+## DeltaMultiplier
+You can use the ```deltaMultiplier``` parameter to control the amount of obfuscation you want.
+```
+var mapObfuscation = require("map-obfuscation");//Include MapObfuscation
+console.log(mapObfuscation.wgs2sd(12.345, 67.890, 0.5));//Print obfuscated coordinates in the console
 ```
--- obfuscate.js
+++ obfuscate.js
@@ -5,9 +5,11 @@ function round(value, precision) {
 	return Math.round(value * multiplier) / multiplier;
 }
 //This function turns WGS84 coordinates and scrambles them. wgsLat is the latitude and wgsLng is the longitude.
-function wgs2sd(wgsLat, wgsLng) {
-	var dlat = Math.sin(Math.sin(Math.sin(Math.sin(Math.sin(Math.sin(Math.sin(Math.sin(wgsLat))))))));//Calculate the delta for the latitude
-	var dlng = Math.cos(Math.cos(Math.cos(Math.cos(Math.cos(Math.cos(Math.cos(Math.cos(wgsLng))))))));//Calculate the delta for the longitude
+function wgs2sd(wgsLat, wgsLng, deltaMultiplier = 1) {
+	//Find the deltas(and multiply them by the deltaMultiplier)
+	var dlat = deltaMultiplier*Math.sin(Math.sin(Math.sin(Math.sin(Math.sin(Math.sin(Math.sin(Math.sin(wgsLat))))))));//Use sine functions to calculate the delta for the latitude
+	var dlng = deltaMultiplier*Math.cos(Math.cos(Math.cos(Math.cos(Math.cos(Math.cos(Math.cos(Math.cos(wgsLng))))))));//Use cosine functions to calculate the delta for the longitude
 	return { "lat": round(wgsLat + dlat, 3), "lng": round(wgsLng + dlng, 3) };//Add the latitude/longitude to their respective deltas. Then round the new values to the 1000ths place
 }
+console.log(wgs2sd(12.345, 67.890));//Test
 module.exports.wgs2sd = wgs2sd;//Export the function
